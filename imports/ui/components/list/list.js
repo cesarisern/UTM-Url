441e11db5e02a3bc0c0efb5b22b8136387d70ef7
cesarisern <cesarisern@gmail.com>
2018-05-06 23:31:28 +0100

    Filtered users
    Only showing content to the user owner of the content

--- imports/ui/components/list/list.js
+++ imports/ui/components/list/list.js
@@ -2,44 +2,14 @@ import { Links } from '/imports/api/links/links.js';
 import { Meteor } from 'meteor/meteor';
 import './list.html';
 
+Template.list.onCreated(function () {
+  Meteor.subscribe('links.all');
+});
 
-
-// Template.form.onCreated(function () {
-//   Meteor.subscribe('links.all');
-// });
-//
-// Template.form.helpers({
-//   links() {
-//     return Links.find({});
-//   },
-// });
-//
-// Template.form.events({
-//   'submit .info-link-add'(event) {
-//     event.preventDefault();
-//
-//     const target = event.target;
-//     const WebsiteURL = target.WebsiteURL;
-//     const Source = target.Source;
-//     const Medium = target.Medium;
-//     const Name = target.Name;
-//     const Term = target.Term;
-//     const Content = target.Content;
-//
-//     Meteor.call('links.insert',
-//       WebsiteURL.value,
-//       Source.value,
-//       Medium.value,
-//       Name.value,
-//       Term.value,
-//       Content.value, (error) => {
-//
-//       if (error) {
-//         alert(error.error);
-//       } else {
-//         title.value = '';
-//         url.value = '';
-//       }
-//     });
-//   },
-// });
+Template.list.helpers({
+  links() {
+    if ( Meteor.userId() ){
+      return Links.find({owner: Meteor.userId() });
+    }
+  },
+});
